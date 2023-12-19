59f2dc3ad8e3038a8ce5a4373d67cccadef6c0b6
Arsheep Mann <asm32@sfu.ca>
2023-12-18 20:33:26 -0800

    Multithreaded access to a bounded buffer

---
+++ build/CMakeFiles/candykids.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/cmpt201/units/03-memory/a11-arshmann2003/src/bbuff.c" "CMakeFiles/candykids.dir/src/bbuff.c.o" "gcc" "CMakeFiles/candykids.dir/src/bbuff.c.o.d"
+  "/home/cmpt201/units/03-memory/a11-arshmann2003/src/candykids.c" "CMakeFiles/candykids.dir/src/candykids.c.o" "gcc" "CMakeFiles/candykids.dir/src/candykids.c.o.d"
+  "/home/cmpt201/units/03-memory/a11-arshmann2003/src/stats.c" "CMakeFiles/candykids.dir/src/stats.c.o" "gcc" "CMakeFiles/candykids.dir/src/stats.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/candykids.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/candykids.dir/src/bbuff.c.o"
+  "CMakeFiles/candykids.dir/src/bbuff.c.o.d"
+  "CMakeFiles/candykids.dir/src/candykids.c.o"
+  "CMakeFiles/candykids.dir/src/candykids.c.o.d"
+  "CMakeFiles/candykids.dir/src/stats.c.o"
+  "CMakeFiles/candykids.dir/src/stats.c.o.d"
+  "candykids"
+  "candykids.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/candykids.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
